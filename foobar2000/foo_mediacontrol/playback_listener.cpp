88480fdd5329a18158ad45ea361709617c208d2a
Jonas van den Berg <jonas@vandenbe.com>
2024-10-11 12:48:14 +0200

    Refactor timeline properties handling

--- foobar2000/foo_mediacontrol/playback_listener.cpp
+++ foobar2000/foo_mediacontrol/playback_listener.cpp
@@ -4,6 +4,27 @@
 #include "media_controls.h"
 #include "track_data.h"
 
+static inline Windows::Foundation::TimeSpan timespan_for_duration(double duration_s)
+{
+	// TimeSpan's contain a time period expressed in 100-nanosecond units.
+	// https://learn.microsoft.com/en-us/uwp/api/windows.foundation.timespan
+	return Windows::Foundation::TimeSpan{ static_cast<long long>(duration_s * 1e7) };
+}
+
+static Windows::Media::SystemMediaTransportControlsTimelineProperties^
+timeline_properties_for_duration(double duration_s, double position_s = 0.0)
+{
+	position_s = max(0.0, position_s);
+	duration_s = max(0.0, duration_s);
+	auto timeline_properties = ref new Windows::Media::SystemMediaTransportControlsTimelineProperties();
+	timeline_properties->Position = timespan_for_duration(position_s);
+	timeline_properties->StartTime = timespan_for_duration(0);
+	timeline_properties->EndTime = timespan_for_duration(duration_s);
+	timeline_properties->MinSeekTime = timespan_for_duration(0);
+	timeline_properties->MaxSeekTime = timespan_for_duration(duration_s);
+	return timeline_properties;
+}
+
 void playback_listener::on_playback_new_track(metadb_handle_ptr p_track) {
 	try {
 		// create new track data from metadb handle
@@ -14,26 +35,16 @@ void playback_listener::on_playback_new_track(metadb_handle_ptr p_track) {
 		m_last_duration = duration;
 
 		// update the media controls
-		auto controls = media_controls::get();
-		controls.begin_update();
-		controls.set_title(data.get_title() != track_data::empty ? data.get_title() : data.get_file_name());
-		controls.set_artist(data.get_artist());
-		controls.set_genres(data.get_genres());
-		controls.set_album(data.get_album());
-		controls.set_track_number(data.get_track_number());
-		controls.set_thumbnail(data.get_album_art());
-		auto timeline_properties = ref new Windows::Media::SystemMediaTransportControlsTimelineProperties();
-		timeline_properties->Position = Windows::Foundation::TimeSpan{ 0 };
-		if (duration > 0) {
-			// TimeSpan's contain a time period expressed in 100-nanosecond units.
-			// https://learn.microsoft.com/en-us/uwp/api/windows.foundation.timespan
-			timeline_properties->StartTime = Windows::Foundation::TimeSpan{ 0 };
-			timeline_properties->EndTime = Windows::Foundation::TimeSpan{ static_cast<long long>(duration * 1e7) };
-			timeline_properties->MinSeekTime = Windows::Foundation::TimeSpan{ 0 };
-			timeline_properties->MaxSeekTime = Windows::Foundation::TimeSpan{ static_cast<long long>(duration * 1e7) };
-		}
-		controls.set_timeline_properties(timeline_properties);
-		controls.end_update();
+		media_controls::get()
+			.begin_update()
+			.set_title(data.get_title() != track_data::empty ? data.get_title() : data.get_file_name())
+			.set_artist(data.get_artist())
+			.set_genres(data.get_genres())
+			.set_album(data.get_album())
+			.set_track_number(data.get_track_number())
+			.set_thumbnail(data.get_album_art())
+			.set_timeline_properties(timeline_properties_for_duration(duration))
+			.end_update();
 	}
 	catch (pfc::exception e) {
 		popup_message::g_show("Caught exception", "Error");
@@ -60,14 +71,6 @@ void playback_listener::on_playback_pause(bool p_state) {
 }
 
 void playback_listener::on_playback_seek(double p_time) {
-	auto timeline_properties = ref new Windows::Media::SystemMediaTransportControlsTimelineProperties();
-	timeline_properties->Position = Windows::Foundation::TimeSpan{ static_cast<long long>(p_time * 1e7) };
-	if (m_last_duration > 0) {
-		timeline_properties->StartTime = Windows::Foundation::TimeSpan{ 0 };
-		timeline_properties->EndTime = Windows::Foundation::TimeSpan{ static_cast<long long>(m_last_duration * 1e7) };
-		timeline_properties->MinSeekTime = Windows::Foundation::TimeSpan{ 0 };
-		timeline_properties->MaxSeekTime = Windows::Foundation::TimeSpan{ static_cast<long long>(m_last_duration * 1e7) };
-	}
 	media_controls::get()
-		.set_timeline_properties(timeline_properties);
+		.set_timeline_properties(timeline_properties_for_duration(m_last_duration, p_time));
 }
